c4faa29c6f25fcc5e6adef9b50ff2020c0ca4c62
KIRTANDEL <98909384+TANDEL1@users.noreply.github.com>
2024-01-11 23:19:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 function isSameType(value1, value2) {
   //your js code here
-	            if(value1 == String)
-            {
-                alert(false);
-            }
-            else
-            {
-                alert(true);
-            }
+	return typeof value1 === typeof value2;
 }
 
 // do not change the code below.
